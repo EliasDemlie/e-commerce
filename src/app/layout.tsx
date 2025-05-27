5c60546bf1606d74698b9487d92f2192105a0238
Elias Demlie <elias.dm257@gmail.com>
2025-05-27 09:12:26 +0300

    error fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Metadata } from 'next'
 import { Toaster } from 'react-hot-toast'
 import { NotificationProvider } from '@/provider/NotificationProvider'
 
+
 export const metadata: Metadata = {
   title: 'Gulit Gebeya',
   description: 'AI-powered e-commerce web app',
